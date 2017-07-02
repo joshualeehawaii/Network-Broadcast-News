09aa11139fc76e89142a4dd8636967117a6c9052
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-01 17:33:26 -1000

    this is the client

---
+++ client.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6*/
+const net = require('net');
+
+//Connect client to server
+const client = net.createConnection({port: 6969, host: '0.0.0.0'}, () => {
+  console.log('connected to server');
+
+});
+
+client.on('connect', () => {
+  process.stdin.pipe(client);
+  //console.log('client inputed data');
+});
+
